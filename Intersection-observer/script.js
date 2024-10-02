7c65e5bd1249ab538c888323e519d94c0d50de1a
ygvbhy <ygvbhy@naver.com>
2024-10-02 23:05:58 +0900

    Intersection observer

---
+++ Intersection-observer/script.js
@@ -0,0 +1,24 @@
+const count = 20;
+let itemIndex = 0;
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    console.log(entries);
+    entries.forEach((entry) => {
+      const list = document.querySelector(".list");
+
+      if (entry.isIntersecting) {
+        for (let i = itemIndex; i < itemIndex + count; i++) {
+          let item = document.createElement("p");
+          item.textContent = i;
+          item.className = "item";
+          list.appendChild(item);
+        }
+        itemIndex += count;
+      }
+    });
+  },
+  { root: null, threshold: 0.1 }
+);
+
+observer.observe(document.querySelector(".end"));
