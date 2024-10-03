1ddcdf55099b2329dc4e514c83a5102710901d65
ygvbhy <ygvbhy@naver.com>
2024-10-03 14:54:02 +0900

    sub class

--- classes/script.js
+++ classes/script.js
@@ -17,3 +17,28 @@ class Person {
 const john = new Person("John", "email@email.com", "10-3-98");
 
 console.log(john);
+console.clear();
+
+// subClass
+class Hu {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+
+  introduce() {
+    return `Hello. my name is ${this.name}`;
+  }
+}
+
+class Client extends Hu {
+  constructor(name, email, phone, address) {
+    super(name, email);
+    this.phone = phone;
+    this.address = address;
+  }
+}
+
+const man = new Client("man", "email@email.com", "01-0101-1010", "서울");
+console.log(man.introduce());
+console.log(man);
